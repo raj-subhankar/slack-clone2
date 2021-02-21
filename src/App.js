3e5eefef90f31b433d5410630a54b694ed483d03
raj-subhankar <subhankar.rj@gmail.com>
2021-02-21 20:01:35 +0530

    Create Sidebar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import { Route, Switch } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import styled from "styled-components";
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="app">
       <Router>
         <>
+        <Header/>
+        <AppBody >
+          <Sidebar />
           <Switch>
             <Route path="/" exact>
-              <Header />
+
             </Route>
           </Switch>
+        </AppBody>
         </>
       </Router>      
     </div>
@@ -21,3 +27,8 @@ function App() {
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
