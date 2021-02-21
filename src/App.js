1ee1e36e301708e20d0c880cf15d8f830b2d0552
raj-subhankar <subhankar.rj@gmail.com>
2021-02-21 17:50:45 +0530

    Create Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
-      
+      <Router>
+        <>
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>      
     </div>
   );
 }
